5cba2deb268883af5a4bf26119a6211407feea8f
Chris Chen <sasowang@gmail.com>
2021-06-21 10:31:56 +0800

    add color palette

--- app/src/main/java/com/chris/android_samples/activity/ColorPaletteActivity.kt
+++ app/src/main/java/com/chris/android_samples/activity/ColorPaletteActivity.kt
@@ -2,11 +2,17 @@ package com.chris.android_samples.activity
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.chris.android_samples.databinding.ActivityColorPaletteBinding
 import com.chris.android_samples.extension.getViewModel
 import com.chris.android_samples.viewmodel.ColorPaletteViewModel
+
 
 class ColorPaletteActivity : AppCompatActivity() {
 
@@ -28,6 +34,21 @@ class ColorPaletteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityColorPaletteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Glide.with(applicationContext)
+            .asBitmap()
+            .load("https://www.google.es/images/srpr/logo11w.png")
+            .into(object : CustomTarget<Bitmap>(SIZE_ORIGINAL, SIZE_ORIGINAL) {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    binding.imageView.setImageBitmap(resource)
+                    viewModel.bitmap = resource
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+            })
 
         binding.startButton.setOnClickListener {
             viewModel.generate()
--- app/src/main/java/com/chris/android_samples/viewmodel/ColorPaletteViewModel.kt
+++ app/src/main/java/com/chris/android_samples/viewmodel/ColorPaletteViewModel.kt
@@ -1,7 +1,52 @@
 package com.chris.android_samples.viewmodel
 
-class ColorPaletteViewModel: BaseViewModel() {
+import android.graphics.Bitmap
+import android.util.Log
+
+
+class ColorPaletteViewModel : BaseViewModel() {
+
+    companion object {
+        const val defaultValue = 20
+    }
+
+    var bitmap: Bitmap? = null
+
+
     fun generate() {
-        //TODO
+        bitmap?.run {
+            val map = mutableMapOf<Int, Int>()
+//            val data = Array(width) {Array(height) {0} }
+
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    // Get the color of a pixel within myBitmap.
+                    val key = getPixel(x, y)
+                    map[key] = (map[key] ?: 0) + 1
+//                    val r: Int = Color.red(pixelColor)
+//                    val g: Int = Color.green(pixelColor)
+//                    val b: Int = Color.blue(pixelColor)
+//                    val t: Int = Color.alpha(pixelColor)
+                }
+            }
+
+            val allList = map.toList().sortedByDescending {
+                it.second
+            }
+
+            val list = allList.take(
+                if (allList.size > defaultValue) {
+                    defaultValue
+                } else {
+                    allList.size
+                }
+            ).toList()
+
+            print(list.size)
+            for (e in list) {
+                Log.e("Chris", "#%06X".format(0xFFFFFF and e.first) + ", total:${e.second}")
+            }
+
+        } ?: return
     }
 }
